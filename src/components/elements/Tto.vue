2927703c0887578664120e4884ebf14021d88cde
JHasselbring <janmykhail.hasselbring@outlook.com>
2021-11-28 03:00:56 +0000

    adds tto

---
+++ src/components/elements/Tto.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tto-container">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.tto-container{
+  overflow: hidden;
+}
+@media screen and (max-width: 640px) {
+  .tto-container {
+    display: block;
+    width: 100%;
+  }
+}
+@media screen and (min-width: 641px) and (max-width: 992px) {
+  .tto-container {
+    display: inline-block;
+    width: 50%;
+  }
+}
+@media screen and (min-width: 993px) {
+  .tto-container {
+    display: inline-block;
+    width: 33%;
+  }
+}
+</style>
